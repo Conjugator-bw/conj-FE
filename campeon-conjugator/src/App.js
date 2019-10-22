05293af1b2644e97ddd273df936de23c88562343
Ben Q <Quackenbush117@gmail.com>
2019-10-21 22:44:28 -0600

    Main verb state now working
    Co-authored-by: Leo-Torres <leo.htor@gmail.com>
    Co-authored-by: Brian-Moir <brianmoir80@gmail.com>
    Co-authored-by: Christian-Auld <christian.martin.auld@gmail.com

--- campeon-conjugator/src/App.js
+++ campeon-conjugator/src/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
-
-class App extends Component {
-    state = {
-        something: "something clever here. Hello World!"
-    }
-
-
-    render() {
-        return (
-            <div>
-               <p>{this.state.something}</p> 
-            </div>
-        )
-    }
-
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Verbo from './components/verbo/verbo.js'
+const App = () => {
+   const [ verbs, setVerbs ] = useState([]);
+   useEffect(() => {
+       const getVerbs =() => {
+           axios
+               .get('https://raw.githubusercontent.com/ghidinelli/fred-jehle-spanish-verbs/master/jehle_verb_lookup.json')
+               .then(response => {
+                   console.log(response)
+               })
+               .catch(error => {
+                   console.log('error')
+               })
+       }
+       getVerbs();
+       }, []);
+       return (
+           <div>
+               <p>something goes here</p>
+              <Verbo />
+           </div>
+       )
 }
-
 export default App;
---
+++ campeon-conjugator/src/components/verbo/verbo.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+
+// function Verbo (props) {
+//     const {
+//       value,
+//       addAccent,
+//       setValue,
+//     } = props;
+
+
+
+const Verbo = (props) => {
+    console.log(props)
+
+    function getRandom(max) {
+        return Math.floor(Math.random()*Math.floor(max));
+    }
+
+    // const [ verbs, setVerbs ] = useState([]);
+    const [inf, setInf] = useState("")
+    const [conj, setConj] = useState({conj: ""})
+    const [def, setDef] = useState("")
+    const [tense, setTense] = useState("")
+    const [perf, setPerf] = useState("")
+    const [mood, setMood] = useState("")
+
+
+    useEffect(() => {
+        
+            axios
+                .get('https://raw.githubusercontent.com/ghidinelli/fred-jehle-spanish-verbs/master/jehle_verb_lookup.json')
+                .then(res => {
+                    // console.log(response)
+                    const vari = Object.entries(res.data);
+                    // console.log(res.data)
+                    const obj = vari[getRandom(1000)];
+                    setConj(obj[0]);
+                    const newArr = obj[1];
+                    const makeArr = newArr[0];
+                    setInf(makeArr.infinitive);
+                    setTense(makeArr.tense);
+                    setDef(makeArr.translation);
+                    setPerf(makeArr.performer);
+                    setMood(makeArr.mood);
+                    console.log(makeArr, "found it")
+                })
+                .catch(error => {
+                    console.log('error')
+                })
+            }, [])
+
+            // console.log("hello", inf)
+           
+      
+     
+        return (
+            <div>
+               <p>"something here"</p>
+               {/* <p>{console.log(makeArr)}</p> */}
+               <p>Infinitive: {inf}</p>
+               <p>Conjugation: {conj.conj}</p>
+               <p>Definition: {def}</p>
+               <p>Tense: {tense}</p>
+               <p>Performer: {perf}</p>
+               <p>Mood: {mood}</p> 
+            
+            </div>
+        )
+        }
+ export default Verbo;
