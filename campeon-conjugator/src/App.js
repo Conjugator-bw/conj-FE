63fb3a9376977de04096e7374283a4b97910f53d
Ben Q <Quackenbush117@gmail.com>
2019-10-21 20:03:39 -0600

    Added something to App.js to deply

--- campeon-conjugator/src/App.js
+++ campeon-conjugator/src/App.js
@@ -1,2 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+class App extends Component {
+    state = {
+        something: "something clever here. Hello World!"
+    }
+
+
+    render() {
+        return (
+            <div>
+               <p>{this.state.something}</p> 
+            </div>
+        )
+    }
+
+
+}
+
+export default App;
