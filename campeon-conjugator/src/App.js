4f4d22b381fd07cbfa43147db4f1d63623caf0aa
osogrizz <lhtor@gmail.com>
2019-10-25 17:53:16 -0700

    Comments out unused imports.

--- campeon-conjugator/src/App.js
+++ campeon-conjugator/src/App.js
@@ -4,12 +4,15 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Footer from './components/verbo/footer';
 import Navbar from './components/navbar.js';
 import Verbo from './components/verbo/verbo.js';
-import SignUp from './components/sign-up/sign-up.js';
-import Login from './components/login/login.js';
 import PrivateRoute from './components/private-route/privateRoute.js';
-import LoginModal from "./components/modal/loginmodal.js";
-import useModal from "./components/modal/useModal.js";
 import styled from 'styled-components';
+
+
+
+// import SignUp from './components/sign-up/sign-up.js';
+// import Login from './components/login/login.js';
+// import LoginModal from "./components/modal/loginmodal.js";
+// import useModal from "./components/modal/useModal.js";
 
 const BodyStyle = styled.div`
     background-color: #e5e3ff;
